0d28eb96eacc800f6bdeca702ef9e2a80eb4d168
JazzPiece <jasur19980708@gmail.com>
2025-03-17 18:12:54 -0700

    AI: Automated update - 2025-03-17 18:12:54

---
+++ ai_work/Chemistry_in_code_collection/src/create_periodic_table_dataframe.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+data = {
+    'symbol': ['H', 'He', 'Li', 'Be', 'B', 'C', 'N', 'O', 'F', 'Ne'],
+    'name': ['Hydrogen', 'Helium', 'Lithium', 'Beryllium', 'Boron', 'Carbon', 'Nitrogen', 'Oxygen', 'Fluorine', 'Neon'],
+    'atomic_number': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+    'atomic_mass': [1.008, 4.0026, 6.94, 9.0122, 10.81, 12.011, 14.007, 15.999, 18.998, 20.180],
+}
+
+periodic_table = pd.DataFrame(data)
+print(periodic_table)
