bcf1ec91c64ddbd392f7546dd996960219357fb3
JazzPiece <jasur19980708@gmail.com>
2025-03-26 19:36:59 -0700

    AI: Automated update - 2025-03-26 19:36:59

---
+++ ai_work/python_map_helper_methods/src/filter_negative_numbers.py
@@ -0,0 +1,2 @@
+def filter_negative_numbers(numbers):
+    return list(filter(lambda x: x < 0, numbers))
---
+++ ai_work/python_map_helper_methods/src/triple_numbers.py
@@ -0,0 +1,2 @@
+def triple_numbers(numbers):
+    return list(map(lambda x: x * 3, numbers))
